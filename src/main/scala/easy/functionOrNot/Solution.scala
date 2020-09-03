e278c2aeace1512a83cf68827f0d179725716589
Gjertsson Konrad <konrad.gjertsson@afconsult.com>
2020-09-03 15:47:15 +0200

    update

--- src/main/scala/easy/functionOrNot/Solution.scala
+++ src/main/scala/easy/functionOrNot/Solution.scala
@@ -5,14 +5,15 @@ class Solution {
   def solve(testCases: Seq[(Int, Int)]): String =
     testCases
       .groupBy { case (input, _) => input }
-      .map { case (_, outputs) => outputs.map { case (_, output) => output } }.toSeq
-      .filter(output => output.length == output.distinct.length)
+      .filter { case (_, outputs) => outputs.map { case (_, output) => output }.distinct.length > 1 }.toSeq
     match {
       case output if output.nonEmpty => "YES"
       case _ => "NO"
     }
 
   def main(args: Array[String]) {
+
+    args
 
   }
 

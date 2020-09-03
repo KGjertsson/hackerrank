75fc8535615bd00332644ff03623f32651876b97
Gjertsson Konrad <konrad.gjertsson@afconsult.com>
2020-09-03 12:58:41 +0200

    teeny weeny progress on a teeny weeny solution

--- src/main/scala/easy/functionOrNot/Solution.scala
+++ src/main/scala/easy/functionOrNot/Solution.scala
@@ -2,7 +2,18 @@ package easy.functionOrNot
 
 class Solution {
 
-  def square(x: Int): Int =
-    x * x
+  def solve(testCases: Seq[(Int, Int)]): String =
+    testCases
+      .groupBy { case (input, _) => input }
+      .map { case (_, outputs) => outputs.map { case (_, output) => output } }.toSeq
+      .filter(output => output.length == output.distinct.length)
+    match {
+      case output if output.nonEmpty => "YES"
+      case _ => "NO"
+    }
+
+  def main(args: Array[String]) {
+
+  }
 
 }
--- src/test/scala/easy/functionOrNot/Tests.scala
+++ src/test/scala/easy/functionOrNot/Tests.scala
@@ -7,10 +7,20 @@ import org.scalatestplus.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class Tests extends AnyFunSuite {
 
-  test("todo") {
+  test("test case 1") {
     def solution = new Solution()
 
-    assert(solution.square(3) == 9)
+    val testCase = "2\n3\n1 1\n2 2\n3 3\n4\n1 2\n2 4\n3 6\n4 8"
+
+    assert(solution.solve(testCase) == "YES\nYES")
+  }
+
+  test("test case 2") {
+    def solution = new Solution()
+
+    val testCase = "2\n3\n1 1\n2 2\n3 3\n4\n1 2\n2 4\n3 6\n4 8"
+
+    assert(solution.solve(testCase) == "YES\nYES")
   }
 
 }

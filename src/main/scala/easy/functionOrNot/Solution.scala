c9d1f24c150ae298b8014ada3ec2767fb3cce9fa
Konrad Gjertsson <gjertsson.konrad@gmail.com>
2020-09-03 21:19:14 +0200

    done with function or not

--- src/main/scala/easy/functionOrNot/Solution.scala
+++ src/main/scala/easy/functionOrNot/Solution.scala
@@ -5,27 +5,27 @@ class Solution {
   def solve(testCases: Seq[(Int, Int)]): String =
     testCases
       .groupBy { case (input, _) => input }
-      .filter { case (_, outputs) => outputs.map { case (_, output) => output }.distinct.length > 1 }.toSeq
-    match {
-      case output if output.nonEmpty => "YES"
-      case _ => "NO"
+      .map { case (_, testCases) => testCases }
+      .filter(testCases => testCases.length > 1) match {
+      case failures if failures.nonEmpty => "NO"
+      case _ => "YES"
     }
 
-  def main(args: Array[String]) {
-
-    args
-
+  def main(args: Array[String]): Unit = {
+    main(io.Source.stdin.getLines().toSeq.reduce(_ + "\n" + _))
   }
 
-  def main(arg: String) {
-    val abc = arg.split("\n").tail.toSeq
+  def main(arg: String): Seq[String] = {
+    arg.split("\n").tail.toSeq
       .foldLeft(Seq.empty[(String, Seq[String])])((acc, element) =>
         element match {
-          case newIndex if newIndex.length == 1 => acc ++ Seq((newIndex, Seq.empty[String]))
-          case testPair => Seq(acc.splitAt(acc.length - 1))
-            .flatMap { case (prev, last) => prev ++ last.map { case (index, testCases) => (index, testCases :+ testPair) } }
+          case newIndex if element.split("\\s+").length == 1 => acc ++ Seq((newIndex, Seq.empty[String]))
+          case testPair => Seq(acc.splitAt(acc.length - 1)).flatMap { case (prev, last) => prev ++ last.map { case (index, testCases) => (index, testCases :+ testPair) } }
         })
-      .map { case (_, testCases) => testCases.flatMap(testCase => testCase.split("\s").map(_.toInt)) }
-      .map(testCases => (testCases.headOption ++ testCases.lastOption)) // need to convert to tuple
+      .map { case (_, testCases) => testCases }
+      .map(testCases => testCases
+        .map(testCase => testCase.split("\\s+").map(_.toInt))
+        .map(testCases => (testCases.head, testCases.last)))
+      .map(solve)
   }
 }

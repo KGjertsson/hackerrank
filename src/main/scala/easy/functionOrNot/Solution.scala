88d6e810a7abd1bc274099dc330b0a8c63544355
Gjertsson Konrad <konrad.gjertsson@afconsult.com>
2020-09-03 19:27:03 +0200

    fix

--- src/main/scala/easy/functionOrNot/Solution.scala
+++ src/main/scala/easy/functionOrNot/Solution.scala
@@ -17,4 +17,15 @@ class Solution {
 
   }
 
+  def main(arg: String) {
+    val abc = arg.split("\n").tail.toSeq
+      .foldLeft(Seq.empty[(String, Seq[String])])((acc, element) =>
+        element match {
+          case newIndex if newIndex.length == 1 => acc ++ Seq((newIndex, Seq.empty[String]))
+          case testPair => Seq(acc.splitAt(acc.length - 1))
+            .flatMap { case (prev, last) => prev ++ last.map { case (index, testCases) => (index, testCases :+ testPair) } }
+        })
+      .map { case (_, testCases) => testCases.flatMap(testCase => testCase.split("\s").map(_.toInt)) }
+      .map(testCases => (testCases.headOption ++ testCases.lastOption)) // need to convert to tuple
+  }
 }
